db440461bf19237701aedad598e0ec303ab3e5ca
Travis T <travistokuyama@gmail.com>
2018-11-07 21:25:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
         @@cart << search_term
         resp.write "added #{search_term}"
       else
-        resp.write "Item Not Found"
+        resp.write "Item Not Found. We don't have that item"
       end
     else
       resp.write "Path Not Found"
